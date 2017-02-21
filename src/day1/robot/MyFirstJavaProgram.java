feafd26663bc55c77989afeabb5acb5584f924fb
yuandrew2014 <yuandrew2014@gmail.com>
2017-02-21 12:07:56 -0800

    Add files via upload

---
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -0,0 +1,21 @@
+package day1.robot;
+
+import java.awt.Color;
+import org.jointheleague.graphical.robot.Robot;
+
+public class MyFirstJavaProgram {
+	public static void main(String[] args) {
+		
+		// START HERE
+Robot idontknow = new Robot("batman");
+idontknow.penDown();
+idontknow.turn(90);
+idontknow.move(100);
+idontknow.turn(90);
+idontknow.move(100);
+idontknow.turn(45);
+idontknow.move(50);
+
+		
+	}
+}
